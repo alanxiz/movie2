fd4d00bfa2d6c7e23186f2c01db7b73013dde598
Alan Zhang <xzhang@rodanandfields.com>
2018-06-18 22:51:11 -0700

    effect of movie year on review count

--- explore_movies.R
+++ explore_movies.R
@@ -86,6 +86,39 @@ s1<- s1 %>%
 movies %>%
   filter(movieId %in% c(2324, 594))
 
+################### check the relationship between length of movie and number of reviews
+
+# substring movie title to get year of movie
+
+library(stringr)
+
+movies<- movies %>%
+  mutate(year = str_sub(title, -5, -2))
+
+# merge s1 with movies to get the year of the movie
+
+s2 <- inner_join(s1, movies, by = "movieId") %>%
+        select(movieId, avg, nsize, year)
+
+s2$year <- as.numeric(s2$year)
+
+# remove NA in year due to conversion
+
+s2<- s2 %>%
+       filter(!is.na(year))
+
+# check correlation between year and review count
+
+cor(s2$nsize, s2$year)  # -0.09
+
+# use regression
+
+model <- lm(nsize ~ year, data = s2)
+summary(model)  # year is not significant 
+
+
+
+
 
 
 
